73f8717e6cc02339c8ad53f5aebcb441e2953daf
Naufal Pinasthika <naufalpinasthika@gmail.com>
2026-05-20 08:29:51 +0700

    feat: ASTBuilder skeleton

--- src/Semantic-Analyzer/ASTBuilder.hpp
+++ src/Semantic-Analyzer/ASTBuilder.hpp
@@ -1,16 +1,69 @@
-class ASTBuilder
-{
+#pragma once
+
+#include "ASTNode.hpp"
+#include "../Parser/Node.hpp"
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class ASTBuilder {
 private:
-    /* data */
+    vector<string> errors;
+
+    bool isTerminal(Node* node) const;
+    bool isToken(Node* node, const string& type) const;
+    string lexeme(Node* node) const;
+
+    Node* findChild(Node* node, NodeType type) const;
+    vector<Node*> findChildren(Node* node, NodeType type) const;
+
+    string getFirstIdent(Node* node) const;
+    string getOperator(Node* node) const;
+
+    ProgramNode* buildProgram(Node* node);
+
+    vector<DeclarationNode*> buildDeclarationPart(Node* node);
+    vector<DeclarationNode*> buildConstDeclaration(Node* node);
+    vector<DeclarationNode*> buildTypeDeclaration(Node* node);
+    vector<DeclarationNode*> buildVarDeclaration(Node* node);
+
+    vector<string> buildIdentifierList(Node* node);
+    ExpressionNode* buildConstant(Node* node);
+
+    TypeNode* buildType(Node* node);
+    TypeNode* buildArrayType(Node* node);
+    TypeNode* buildRangeType(Node* node);
+    TypeNode* buildRecordType(Node* node);
+
+    StatementNode* buildCompoundStatement(Node* node);
+    vector<StatementNode*> buildStatementList(Node* node);
+    StatementNode* buildStatement(Node* node);
+
+    StatementNode* buildAssignment(Node* node);
+    StatementNode* buildIf(Node* node);
+    StatementNode* buildWhile(Node* node);
+    StatementNode* buildFor(Node* node);
+    StatementNode* buildRepeat(Node* node);
+    StatementNode* buildCallStatement(Node* node);
+
+    ExpressionNode* buildExpression(Node* node);
+    ExpressionNode* buildSimpleExpression(Node* node);
+    ExpressionNode* buildTerm(Node* node);
+    ExpressionNode* buildFactor(Node* node);
+    ExpressionNode* buildVariable(Node* node);
+
+    vector<ExpressionNode*> buildParameterList(Node* node);
+    vector<ExpressionNode*> buildIndexList(Node* node);
+
+    void addError(const string& message);
+
 public:
-    ASTBuilder(/* args */);
-    ~ASTBuilder();
+    ASTBuilder() = default;
+
+    ProgramNode* build(Node* parseTree);
+
+    bool hasErrors() const;
+    const vector<string>& getErrors() const;
 };
-
-ASTBuilder::ASTBuilder(/* args */)
-{
-}
-
-ASTBuilder::~ASTBuilder()
-{
-}
