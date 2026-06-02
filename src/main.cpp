fdbb6189b75b6a093b7d3dc7160bcc5b73f60f3a
Naufal Pinasthika <naufalpinasthika@gmail.com>
2026-06-02 07:02:11 +0700

    feat: integrate main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "2-Parser/Parser.hpp"
 #include "3-Semantic-Analyzer/ASTBuilder.hpp"
 #include "3-Semantic-Analyzer/Semantic.hpp"
+#include "4-Intermediate-Code-Generation/CodeGenerator.hpp"
 
 using namespace std;
 
@@ -35,6 +36,7 @@ int main(int argc, char *argv[])
     std::filesystem::create_directories("test/milestone-1");
     std::filesystem::create_directories("test/milestone-2");
     std::filesystem::create_directories("test/milestone-3");
+    std::filesystem::create_directories("test/milestone-4");
 
     string mode = "S"; // default semantic analyzer
     string s;
@@ -58,17 +60,20 @@ int main(int argc, char *argv[])
         } else if (flag == "-S")
         {
             mode = "S";
+        } else if (flag == "-I")
+        {
+            mode = "I";
         }
         else
         {
             cerr << "Unknown flag: " << flag << endl;
-            cerr << "Usage: " << argv[0] << " [ -L | -P | -S ] <file>" << endl;
+            cerr << "Usage: " << argv[0] << " [ -L | -P | -S | -I ] <file>" << endl;
             return 1;
         }
     }
     else
     {
-        cerr << "Usage: " << argv[0] << " [ -L | -P | -S ] <file>" << endl;
+        cerr << "Usage: " << argv[0] << " [ -L | -P | -S | -I ] <file>" << endl;
         return 1;
     }
 
@@ -202,6 +207,12 @@ int main(int argc, char *argv[])
     {
         for (const string& error : semantic.getErrors())
             cout << error << endl;
+
+        if (mode == "I")
+        {
+            delete ast;
+            return 1;
+        }
     }
 
     ofstream astOutput("test/milestone-3/" + outputName + "_ast");
@@ -212,6 +223,23 @@ int main(int argc, char *argv[])
     ast->print();
 
     cout.rdbuf(oldCout);
+
+    if (mode == "I")
+    {
+        CodeGenerator generator;
+        const TacProgram& program = generator.generate(ast, semantic.getSymbolTable());
+
+        ofstream icOutput("test/milestone-4/" + outputName + "_ic");
+        if (!icOutput.is_open())
+        {
+            cerr << "Failed to open intermediate code output file" << endl;
+            delete ast;
+            return 1;
+        }
+
+        program.print(cout);
+        program.print(icOutput);
+    }
 
     delete ast;
     return 0;
